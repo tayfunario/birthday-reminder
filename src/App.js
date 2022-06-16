48c1c41d3db4b9e5d7bdb039a87d08bcfa260d45
tayfunetta <tayfunemrekahraman@gmail.com>
2022-06-17 00:12:53 +0300

    my birthday-reminder app with reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.scss';
+import Person1 from './images/person-1.jpg';
+import Person2 from './images/person-2.jpg';
+import Person3 from './images/person-3.jpg';
+import Person4 from './images/person-4.jpg';
+import Person5 from './images/person-5.jpg';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.loadItems = this.loadItems.bind(this)
+    this.createName = this.createName.bind(this)
+    this.clearItems = this.clearItems.bind(this)
+    this.removeItem = this.removeItem.bind(this)
+  }
+
+  createName() {
+    const adject = ['türkiyeli', 'zaferli', 'militarist', 'y-chpli', 'siyasal islamcı', 'kemalist', 'merkez-sağcı', 'liberal', 'komanist', 'taliban-sever']
+    const name = ['canan', 'hilal', 'ebru', 'çetin', 'selo', 'hasan', 'muhammet', 'aylin', 'bora', 'selen']
+    const persons = [Person1, Person2, Person3, Person4, Person5]
+    const num1 = Math.floor(Math.random() * 10)
+    const num2 = Math.floor(Math.random() * 10)
+    let fullName = adject[num1] + ' ' + name[num2]
+
+    const age = Math.floor(Math.random() * 20) + 10
+
+    const person = persons[Math.floor(Math.random() * 5)]
+
+    return { fullName, age, person }
+  }
+
+  clearItems() {
+    const items = document.querySelectorAll('li')
+    for (let i of items) {
+      i.remove()
+    }
+  }
+
+  loadItems() {
+    return <>
+      <Item person={this.createName().person} name={this.createName().fullName} age={this.createName().age} removeItems={this.removeItem} />
+      <Item person={this.createName().person} name={this.createName().fullName} age={this.createName().age} removeItems={this.removeItem} />
+      <Item person={this.createName().person} name={this.createName().fullName} age={this.createName().age} removeItems={this.removeItem} />
+      <Item person={this.createName().person} name={this.createName().fullName} age={this.createName().age} removeItems={this.removeItem} />
+      <Item person={this.createName().person} name={this.createName().fullName} age={this.createName().age} removeItems={this.removeItem} />
+      <Item person={this.createName().person} name={this.createName().fullName} age={this.createName().age} removeItems={this.removeItem} />
+    </>
+  }
+
+  removeItem(event){
+    event.target.parentNode.remove()
+  }
+
+  render() {
+    return (
+      <div id='container'>
+        <div className='reminder'>
+          <h1>Birthday Reminder</h1>
+          <ul>
+            {this.loadItems()}
+          </ul>
+          <button id='clear' onClick={this.clearItems}>CLEAR ALL</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const Item = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <li>
+      <img src={props.person} alt='person'></img>
+      <div>
+        <div className='name'>{props.name}</div>
+        <div className='age'>{props.age} years</div>
+      </div>
+      <i className="fa-solid fa-xmark" onClick={props.removeItems}></i>
+    </li>
+  )
 }
 
 export default App;
